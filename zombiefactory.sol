47692c8344f09bd6d4a7c29e14155ba36fba970f
Nacer-Foual-Set-In-Stone <nacer.foual@setinstone.io>
2021-10-12 09:41:28 +0000

    zombiefactory

---
+++ zombiefactory.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+    
+    uint dnaDigits = 16;
+        uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+            string name;
+	            uint dna;
+		        }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+        mapping (address => uint) ownerZombieCount;
+	/*
+	Goal :
+	Step 1 : récupéré l'id du nouveau zombie
+	Step 2 : met à jour le mappage zombieToOwner pour stocker msg.sender sous cet id.
+	Step 3 : Augmenter le nombre de zombie obtenue du msg,sender
+	Step 4 : Fonction interne afin qu'on puisse l'appeler dans zombefeeding.sol
+	*/
+
+    function _createZombie(string _name, uint _dna) internal {
+            uint id = zombies.push(Zombie(_name, _dna)) - 1;
+	            zombieToOwner[id] = msg.sender;
+		            ownerZombieCount[msg.sender]++;
+			            NewZombie(id, _name, _dna);
+				        }
+					/*
+					Goal :
+					Step 1 : générer un nombre aléatoire hexadécimal,
+					Step 2 : le convertir en un uint
+					Step 3 : stocker le résultat dans un uint nommé rand.
+					*/
+					    function _generateRandomDna(string _str) private view returns (uint) {
+					            uint rand = uint(keccak256(_str));
+						            return rand % dnaModulus;
+							        }
+
+
+/*
+Goal :
+Step 1 : vérifie si l'address ne contient pas de zombie
+Step 2 : génére un nouveau zombie
+Step 3 : Appel l'événement _createZombie
+*/
+    function createRandomZombie(string _name) public {
+            require(ownerZombieCount[msg.sender] == 0);
+	            uint randDna = _generateRandomDna(_name);
+		            _createZombie(_name, randDna);
+			        }
+				    }
+
+}
+
+
+
+
+
+
+
