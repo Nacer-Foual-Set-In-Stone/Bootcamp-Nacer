35259412ef5497bbb157a3030c2314cd46669671
Nacer-Foual-Set-In-Stone <nacer.foual@setinstone.io>
2021-10-12 14:46:46 +0000

    zombiefeeding

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,61 +1,66 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.8.9;
 
 contract ZombieFactory {
 
     event NewZombie(uint zombieId, string name, uint dna);
-    
+
     uint dnaDigits = 16;
-        uint dnaModulus = 10 ** dnaDigits;
+    uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
-            string name;
-	            uint dna;
-		        }
+        string name;
+        uint dna;
+        uint32 level;
+        uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
+    }
 
     Zombie[] public zombies;
 
     mapping (uint => address) public zombieToOwner;
-        mapping (address => uint) ownerZombieCount;
-	/*
-	Goal :
-	Step 1 : récupéré l'id du nouveau zombie
-	Step 2 : met à jour le mappage zombieToOwner pour stocker msg.sender sous cet id.
-	Step 3 : Augmenter le nombre de zombie obtenue du msg,sender
-	Step 4 : Fonction interne afin qu'on puisse l'appeler dans zombefeeding.sol
-	*/
+    mapping (address => uint) ownerZombieCount;
+/*
+Goal : 
+Step 1 : récupéré l'id du nouveau zombie
+Step 2 : met à jour le mappage zombieToOwner pour stocker msg.sender sous cet id.
+Step 3 : Augmenter le nombre de zombie obtenue du msg,sender
+Step 4 : Fonction interne afin qu'on puisse l'appeler dans zombefeeding.sol
+*/
 
     function _createZombie(string _name, uint _dna) internal {
-            uint id = zombies.push(Zombie(_name, _dna)) - 1;
-	            zombieToOwner[id] = msg.sender;
-		            ownerZombieCount[msg.sender]++;
-			            NewZombie(id, _name, _dna);
-				        }
-					/*
-					Goal :
-					Step 1 : générer un nombre aléatoire hexadécimal,
-					Step 2 : le convertir en un uint
-					Step 3 : stocker le résultat dans un uint nommé rand.
-					*/
-					    function _generateRandomDna(string _str) private view returns (uint) {
-					            uint rand = uint(keccak256(_str));
-						            return rand % dnaModulus;
-							        }
-
-
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
+    }
+/*    
+Goal : 
+Step 1 : générer un nombre aléatoire hexadécimal, 
+Step 2 : le convertir en un uint
+Step 3 : stocker le résultat dans un uint nommé rand.
+*/
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+    
+    
 /*
-Goal :
+Goal : 
 Step 1 : vérifie si l'address ne contient pas de zombie
 Step 2 : génére un nouveau zombie
 Step 3 : Appel l'événement _createZombie
 */
     function createRandomZombie(string _name) public {
-            require(ownerZombieCount[msg.sender] == 0);
-	            uint randDna = _generateRandomDna(_name);
-		            _createZombie(_name, randDna);
-			        }
-				    }
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+    }
 
-}
+
 
 
 
--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,68 +1,95 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.8.9;
 
 import "./zombiefactory.sol";
 
 
  /*
- Goal : Crée une interface KittyInterface afin de pouvoir appeler la fonction getKitty et récuperer les donnée d'un autre contract
- Step 1 :
- */
- contract KittyInterface {
- function getKitty(uint256 _id) external view returns (
-     bool isGestating,
-         bool isReady,
-	     uint256 cooldownIndex,
-	         uint256 nextActionAt,
-		     uint256 siringWithId,
-		         uint256 birthTime,
-			     uint256 matronId,
-			         uint256 sireId,
-				     uint256 generation,
-				         uint256 genes
-					 );
-					 }
+Goal : Crée une interface KittyInterface afin de pouvoir appeler la fonction getKitty et récuperer les donnée d'un autre contract
+Step 1 : 
+*/
+contract KittyInterface {
+function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+);
+}
 
 
 contract ZombieFeeding is ZombieFactory {
-/*
-Goal : crée une KittyInterface nommée kittyContract, et l'initialise avec ckAddress
-*/
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-        KittyInterface kittyContract = KittyInterface(ckAddress);
+    
+   KittyInterface kittyContract;
+   
+   /*
+   Goal : Condition pour vérifier le propriétaire du zombie
+   */
+    modifier ownerOf(uint _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    _;
+  }
 
+    /*
+    Goal :  fonctions qui nous permet de mettre à jour l'address des kity 
+    */
+   function setKittyContractAddress(address _address) external {
+    kittyContract = KittyInterface(_address);
+  }
+  
+  
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+    
  /*
- Goal :
- Step 1 : vérifie que msg.sender est égal au propriétaire du zombie
- Step 2 : Crée un zombie local associé à l'index _zombieId de notre tableau Zombie
- Step 3 : vérifie que  que _targetDna n'est pas plus long que 16 chiffres grâce à dnaModulus
- Step 4 : Crée un newDna qui est la moyenne entre l'adn du zombie et de la cible
- Step 5 : if pour comparer le hachage keccak256 de _species et la chaîne de caractère "kitty".
- Step 6 : remplace les 2 derniers chiffres de l'ADN par 99
- Step 7 : Crée un zombe 'NoName' avec le nouvelle ADN
- */
-   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
-       require(msg.sender == zombieToOwner[_zombieId]);
-           Zombie storage myZombie = zombies[_zombieId];
-	       _targetDna = _targetDna % dnaModulus;
-	           uint newDna = (myZombie.dna + _targetDna) / 2;
-		       // ajoutez une déclaration `if` ici
-		           if (keccak256(_species) == keccak256("kitty")) {
-			           newDna = newDna - newDna % 100 + 99;
-				       }
-				           _createZombie("NoName", newDna);
-					     }
+Goal : 
+Step 1 : vérifie que msg.sender est égal au propriétaire du zombie
+Step 2 : Crée un zombie local associé à l'index _zombieId de notre tableau Zombie 
+Step 3 : vérifie que  que _targetDna n'est pas plus long que 16 chiffres grâce à dnaModulus
+Step 4 : Crée un newDna qui est la moyenne entre l'adn du zombie et de la cible
+Step 5 : if pour comparer le hachage keccak256 de _species et la chaîne de caractère "kitty".
+Step 6 : remplace les 2 derniers chiffres de l'ADN par 99
+Step 7 : Crée un zombe 'NoName' avec le nouvelle ADN
+*/
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) { //la fonction appelle ownerOf pour vérifier l'appartenacne du zombie
+    //require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
+    _triggerCooldown(myZombie);
+
+  }
+
 
   /*
-  Goal :  fonction qui récupère les gènes d'un chaton à partir du contrat
-  Step 1 : Appele la fonction kittyContract.getKitty avec _kittyId et stocke les genes dans kittyDna.
-  Step 2 : la fonction appele feedAndMultiply avec _zombieId et kittyDna.
-  */
-
+Goal :  fonction qui récupère les gènes d'un chaton à partir du contrat 
+Step 1 : Appele la fonction kittyContract.getKitty avec _kittyId et stocke les genes dans kittyDna.
+Step 2 : la fonction appele feedAndMultiply avec _zombieId et kittyDna.
+*/
+  
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
-      uint kittyDna;
-          (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-	      // et modifiez l'appel de la fonction ici :
-	          feedAndMultiply(_zombieId, kittyDna, "kitty");
-		    }
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    // et modifiez l'appel de la fonction ici :
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
 
 }
+
+
